71054f2ac0ad3d858abcc7d04a01c4dd7ba6e277
salma1232 <salmaeddachri94@gmail.com>
2025-05-27 22:56:12 +0200

    mise à jour

--- train_model.py
+++ train_model.py
@@ -17,7 +17,7 @@ X = vectorizer.fit_transform(df['text'])
 y = df['label']
 
 # Séparer les données
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
 # Entraîner le modèle SVM
 model = LinearSVC()
